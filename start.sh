c60265a9fde504d067fbc76757c7ce30b04f656c
Cyjan <MrCyjaneK@Gmail.com>
2019-08-29 15:10:48 +0200

    Initial version

---
+++ start.sh
@@ -0,0 +1,30 @@
+clear
+echo "==== manubet ====";
+echo "Select method";
+echo " (1) Manual";
+#echo " (2) Automatic";
+echo " (x) Exit";
+read choi
+if [ "$choi" == "1" -o "$choi" == "x" ];
+then
+    echo "OK!";
+else
+    echo "Wrong choice.";
+    exit 1;
+fi
+if [ "$choi" == "1" ];
+then
+    echo "Starting manual.sh";
+    cd /home/`whoami`/TPM/manubet/
+    echo "==== START ====";
+    bash manual.sh
+    echo "==== STOP ====";
+    echo " [ExitCode]: $?";
+    sleep 3
+    exit $?
+fi
+if [ "$choi" == "x" ];
+then
+    echo "Bye";
+    exit 0;
+fi
