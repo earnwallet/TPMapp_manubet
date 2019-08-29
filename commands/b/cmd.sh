b05ed9ae347e14c889aa67b76fd0dd14e848cfd9
Cyjan <MrCyjaneK@Gmail.com>
2019-08-30 01:00:46 +0200

    bugfix

--- commands/b/cmd.sh
+++ commands/b/cmd.sh
@@ -1,4 +1,12 @@
-resp=$(curl /?action=bet&amount=`echo "$1 * 100000000" | bc -l `&chance=$2&bethi=$3);
+lo='lo';
+if [ "$3" == 'lo' ];
+then
+    lo='lo';
+else
+    lo='hi';
+fi
+cd /home/`whoami`/TPM/apps/manubet/config
+resp=$(curl `cat host.txt`:`cat port.txt`/?action=bet&amount=`echo "$1 * 100000000" | bc -l `&chance=$2&bethi=$hi);
 ec=$?
 echo $resp;
 exit $ec;
