a1ca4bd14e544b9871dbad214def46556d73aeae
Cyjan <MrCyjaneK@Gmail.com>
2019-08-30 01:03:48 +0200

    Quote

--- commands/b/cmd.sh
+++ commands/b/cmd.sh
@@ -6,7 +6,7 @@ else
     lo='hi';
 fi
 cd /home/`whoami`/TPM/apps/manubet/config
-resp=$(curl `cat host.txt`:`cat port.txt`/?action=bet&amount=`echo "$1 * 100000000" | bc -l `&chance=$2&bethi=$hi);
+resp=$(curl "`cat host.txt`:`cat port.txt`/?action=bet&amount=`echo "$1 * 100000000" | bc -l `&chance=$2&bethi=$hi");
 ec=$?
 echo $resp;
 exit $ec;
