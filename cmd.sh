47a1b1d9037808659b407b76dc939bff6e6fbb66
Cyjan <MrCyjaneK@Gmail.com>
2019-08-30 00:50:07 +0200

    debug info for commands

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,10 @@
 cd /home/`whoami`/TPM/apps/manubet/commands/$1/
 bash cmd.sh $2 $3 $4 $5 $6 $7 $8 $9
+ec=$?;
+if [ "$ec" == "0" ];
+then
+    echo "[[[SUCCESS]]]";
+else
+    echo "Non-zero exit code occured, [$ec]";
+fi
 #TODO: make it better...
