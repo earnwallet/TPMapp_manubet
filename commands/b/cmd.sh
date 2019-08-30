3beedd1c8a91d8f4a0664b8fc4aa1391d029c9a9
Cyjan <MrCyjaneK@Gmail.com>
2019-08-30 15:36:43 +0200

    define host varible

--- commands/b/cmd.sh
+++ commands/b/cmd.sh
@@ -6,7 +6,8 @@ else
     lo='hi';
 fi
 cd /home/`whoami`/TPM/apps/manubet/config
-resp=$(curl "`cat host.txt`:`cat port.txt`/?action=bet&amount=`echo "$1 * 100000000" | bc -l `&chance=$2&bethi=$hi");
+url="`cat host.txt`:`cat port.txt`;
+resp=$(curl "$url/?action=bet&amount=`echo "$1 * 100000000" | bc -l `&chance=$2&bethi=$hi");
 ec=$?
 echo $resp;
 exit $ec;
