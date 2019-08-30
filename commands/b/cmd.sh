0bd01ee19714a472868772aaf8cf310a6825f395
Cyjan <MrCyjaneK@Gmail.com>
2019-08-30 15:38:52 +0200

    Define amt

--- commands/b/cmd.sh
+++ commands/b/cmd.sh
@@ -6,8 +6,9 @@ else
     lo='hi';
 fi
 cd /home/`whoami`/TPM/apps/manubet/config
-url="`cat host.txt`:`cat port.txt`;
-resp=$(curl "$url/?action=bet&amount=`echo "$1 * 100000000" | bc -l `&chance=$2&bethi=$hi");
+url="`cat host.txt`:`cat port.txt`";
+amt="`echo "$1 * 100000000" | bc -l `";
+resp=$(curl "$url/?action=bet&amount=$amt&chance=$2&bethi=$hi");
 ec=$?
 echo $resp;
 exit $ec;
