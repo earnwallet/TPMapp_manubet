2ceb6479260a2974355c00ff6fc3854aef798d4d
Cyjan <MrCyjaneK@Gmail.com>
2019-08-30 18:44:12 +0200

    Yea final

--- commands/b/pp.php
+++ commands/b/pp.php
@@ -2,5 +2,5 @@
 //print_r($argv);
 $res = json_decode($argv[1]);
 print_r($res);
-echo "\nPayout : ".($res->win/100000000)
-echo "\nBalance: ".($res->balance/100000000)
+echo "\nPayout : ".($res->win/100000000);
+echo "\nBalance: ".($res->balance/100000000);
