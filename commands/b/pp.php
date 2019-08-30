f075b597c03d21486e4e7e91baaac1f29edb0bce
Cyjan <MrCyjaneK@Gmail.com>
2019-08-30 18:45:13 +0200

    👻

--- commands/b/pp.php
+++ commands/b/pp.php
@@ -3,4 +3,4 @@
 $res = json_decode($argv[1]);
 print_r($res);
 echo "\nPayout : ".($res->win/100000000);
-echo "\nBalance: ".($res->balance/100000000);
+echo "\nBalance: ".($res->balance/100000000)."\n";
