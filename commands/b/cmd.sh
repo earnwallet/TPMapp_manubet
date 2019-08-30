8a2993f56714fefe35a573865b1e55a2c2f3314c
Cyjan <MrCyjaneK@Gmail.com>
2019-08-30 18:39:25 +0200

    Update cmd.sh to use pp.php instead of echo

--- commands/b/cmd.sh
+++ commands/b/cmd.sh
@@ -10,5 +10,6 @@ url="`cat host.txt`:`cat port.txt`";
 amt="`echo "$1 * 100000000" | bc -l `";
 resp=$(curl "$url/?action=bet&amount=$amt&chance=$2&bethi=$hi");
 ec=$?
-echo $resp;
+cd /home/`whoami`/TPM/apps/manubet/commands/b/
+php pp.php $resp;
 exit $ec;
